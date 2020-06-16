1f3d528d240acd324561ac6b13ecdaf7f84b5e5c
Taras Ivashchuk <taras@tarasivashchuk.com>
2020-06-16 04:07:23 -0400

    Removed shutil, added logging

--- src/maintenance/__init__.py
+++ src/maintenance/__init__.py
@@ -1,4 +1,5 @@
 from pathlib import Path
 
+
 root_dir = Path("/Users/taras")
 config_dir = root_dir.joinpath("Common Code").joinpath("config")
--- src/maintenance/clean.py
+++ src/maintenance/clean.py
@@ -1,33 +1,41 @@
 """Common common_code."""
 
-import shutil
-
 from pathlib import Path
-from time import time
 from typing import List
+from typing import Optional
 
-from tqdm.auto import tqdm
+import logzero
 
-import common_code
+from alive_progress import alive_bar
 
 
-def clean(location: Path, patterns: List[str]):
+def clean(location: Path, patterns: List[str], logger: Optional[logzero.logger] = None):
     """Clean the target location of all patterns."""
-
-    dirs_progress = tqdm(desc="Directories")
-    files_progress = tqdm(desc="Files")
-
-    start = time()
+    dirs_progress = alive_bar(title="Directories")
+    files_progress = alive_bar(title="Files")
     for pattern in patterns:
-        for match in location.rglob(f"**/{pattern}"):
-            if match.is_dir():
-                shutil.rmtree(match.as_posix())
-                dirs_progress.update()
-            else:
-                match.unlink()
-                files_progress.update()
-    end = time()
-    print(f"Pattern matching successfully completed in {int(end-start)} seconds!")
+        for match in location.rglob(pattern):
+            try:
+                match_type = clean_match(match)
+                dirs_progress.send(1) if match_type == "dir" else files_progress.send(1)
+                if logger is not None:
+                    logger.info(f"Successfully cleaned: {match}")
+            except Exception as e:
+                if logger is not None:
+                    logger.error(f"For {match}: {e}")
+                raise e
+    dirs_progress.close()
+    files_progress.close()
+    return True
+
+
+def clean_match(match):
+    if match.is_dir():
+        match.rmdir(match)
+        return "dir"
+    else:
+        match.rm()
+        return "file"
 
 
 def parse_args():
@@ -38,43 +46,53 @@ def parse_args():
 
     parser.add_argument(
         "--config",
-        "-C",
         required=False,
         type=str,
-        default=".clean",
+        default="/Users/taras/.config/clean.maintenance",
         help="Name of the config file with patterns to use for matching and removing",
     )
     parser.add_argument(
-        "--patterns",
-        "-P",
+        "--location",
+        "-l",
+        type=str,
+        required=False,
+        default=str(Path().cwd().resolve()),
+        help="Location from which to recursively search for patterns",
+    )
+    parser.add_argument(
+        "--log",
+        type=bool,
+        required=False,
+        default=False,
+        help="Whether to log the cleaned files and directories",
+    )
+    parser.add_argument(
+        "--extra-patterns",
+        "-e",
         nargs="*",
         required=False,
         help="Any extra matching patterns not in the configuration file",
     )
-    parser.add_argument(
-        "--location",
-        "-L",
-        type=str,
-        required=False,
-        default="/Users/taras",
-        help="Location from which to recursively search for patterns",
-    )
-
     return parser.parse_args()
+
+
+def load_patterns_config(path: Path) -> List[str]:
+    if not path.exists():
+        raise FileNotFoundError("No cleaning configuration file found!")
+    with path.open() as f:
+        cleaning_patterns = [pattern.strip() for pattern in f.readlines()]
+    return cleaning_patterns
 
 
 def main():
     """Run the module."""
     args = parse_args()
-    clean_config = common_code.config_dir.joinpath(args.config)
-    if not clean_config.exists():
-        raise FileNotFoundError("No cleaning configuration file found!")
-    with clean_config.open() as f:
-        cleaning_patterns = [pattern.strip() for pattern in f.readlines()]
-    if args.patterns:
-        [cleaning_patterns.append(pattern) for pattern in args.patterns]
+    cleaning_patterns = load_patterns_config(Path(args.config))
     location = Path(args.location)
-    clean(location, cleaning_patterns)
+    logger = logzero.setup_logger(name="maintenance-clean") if args.log else None
+    if args.extra_patterns:
+        [cleaning_patterns.append(pattern) for pattern in args.extra_patterns]
+    return clean(location, cleaning_patterns, logger)
 
 
 if __name__ == "__main__":
