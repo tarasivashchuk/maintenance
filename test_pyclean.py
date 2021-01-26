8af5a3f95f434fd87f25af2241b5ed664bc37245
Taras Ivashchuk <taras@tarasivashchuk.com>
2021-01-26 13:23:40 -0500

    test: added empty tests as placeholders

---
+++ test_pyclean.py
@@ -0,0 +1,22 @@
+def test_search():
+    assert False
+
+
+def test_delete():
+    assert False
+
+
+def test_clean():
+    assert False
+
+
+def test_main():
+    assert False
+
+
+def test_parse_root_arg():
+    assert False
+
+
+def test_run():
+    assert False
